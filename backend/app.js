168e3b03326ade935629d63fdc5a1dfd22339401
Gulcan COSKUN <gulcancoskun.a@gmail.com>
2024-12-11 09:44:53 +0100

    connection to mongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,13 @@
 const express = require("express");
 const app = express();
 
+// connect to monoDB
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PSWD}@${process.env.DB_CLUSTER}.mongodb.net/`,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+).then(() => console.log("✅ Connection to MongoDB is successful !")).catch(() => console.log("⛔️ Connection to MongoDB failed!"));
+
 module.exports = app;
