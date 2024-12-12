47cf263aaf28fe0779498763ccb064f699661f21
Gulcan COSKUN <gulcancoskun.a@gmail.com>
2024-12-12 16:24:42 +0100

    create multer-config

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,11 +2,12 @@ const express = require("express");
 const router = express.Router();
 const userControllers = require("../controllers/user");
 const auth = require("../middleware/auth")
+const multer = require("../middleware/multer-config")
 
 // http://localhost:3000/api/auth/signup
 router.post("/signup", userControllers.signup)
 // http://localhost:3000/api/auth/login
 router.post("/login", userControllers.login)
 // http://localhost:3000/api/auth/:id
-router.put("/:id", auth, userControllers.updateUser)
+router.put("/:id", auth, multer, userControllers.updateUser)
 module.exports = router;
