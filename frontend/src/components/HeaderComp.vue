d0a1491fbdaa00858e57501ac7b1be3e4eb3e622
Gulcan COSKUN <gulcancoskun.a@gmail.com>
2024-12-13 15:45:40 +0100

    create logo

--- frontend/src/components/HeaderComp.vue
+++ frontend/src/components/HeaderComp.vue
@@ -1,9 +1,10 @@
 <template>
-  <header>
+  <header class= "m-auto">
     <img
       id="logo"
       alt="header logo"
       src="../assets/icon-left-font-monochrome-black.svg"
+      class = "mt-5 mb-5"
     />
   </header>
 </template>
@@ -11,8 +12,12 @@
 
 <style scoped>
 
-img { width: 40%;
+img { width: 50%;
 }
-
+@media (max-width: 992px) {
+    img {
+        width: 50%
+    }
+}
 
 </style>
