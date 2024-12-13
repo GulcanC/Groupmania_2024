d5681061907ef9096dc83826b2457f03e286cb55
Gulcan COSKUN <gulcancoskun.a@gmail.com>
2024-12-13 15:30:11 +0100

    add homepage and logo for header

---
+++ frontend/src/components/HeaderComp.vue
@@ -0,0 +1,18 @@
+<template>
+  <header>
+    <img
+      id="logo"
+      alt="header logo"
+      src="../assets/icon-left-font-monochrome-black.svg"
+    />
+  </header>
+</template>
+
+
+<style scoped>
+
+img { width: 40%;
+}
+
+
+</style>
